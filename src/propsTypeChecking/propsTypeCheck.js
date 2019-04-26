ddad00204259cdaa71f07a59576c691aff6ab0d4
Shivakumar <shivasorab@gmail.com>
2019-04-26 21:33:11 +0530

    props types checking

---
+++ src/propsTypeChecking/propsTypeCheck.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class PropTypeCheck extends React {
+	
+	render(){
+		return(
+			<div className="App">
+
+			</div>
+		)
+	}
+}
